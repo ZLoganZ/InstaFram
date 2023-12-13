f53e6f63f2d7260fa912fdf604bc5ff020d6c571
ZLoganZ <ln26805@gmail.com>
2023-12-13 13:02:53 +0700

    Refactor Loader component to improve readability and add animation

--- src/components/Shared/Loader.tsx
+++ src/components/Shared/Loader.tsx
@@ -1,7 +1,11 @@
 const Loader = () => {
   return (
-    <div className='flex-center w-full animate-spin'>
-      <img src='/assets/icons/loader.svg' alt='loader' className='w-6 h-6 invert dark:invert-0' />
+    <div className='flex-center w-full'>
+      <img
+        className='w-6 h-6 invert dark:invert-0 animate-spin'
+        src='/assets/icons/loader.svg'
+        alt='loader'
+      />
     </div>
   );
 };
