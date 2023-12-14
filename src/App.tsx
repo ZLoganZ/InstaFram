e91bd3e91b7e99801dddf36247e39c72c3737a49
ZLoganZ <ln26805@gmail.com>
2023-12-15 02:58:02 +0700

    Update dependencies and refactor router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,12 @@
-import { RouterProvider, Router } from '@tanstack/react-router';
+import { RouterProvider } from '@tanstack/react-router';
 
 import { AuthProvider } from '@/providers/AuthProvider';
 import { ThemeProvider } from '@/providers/ThemeProvider';
 
-import { rootRoute, queryClient } from '@/routes/root.routes';
-import {
-  MainRoute,
-  HomeRoute,
-  ExploreRoute,
-  PeopleRoute,
-  CreatePostRoute,
-  PostDetailsRoute,
-  ProfileRoute,
-  UserPostsRoute,
-  LikedPostsRoute,
-  SavedPostsRoute
-} from '@/routes/private.routes';
-import {
-  AuthRoute,
-  SigninRoute,
-  SignupRoute,
-  NotFoundRoute,
-  ForgotPasswordRoute,
-  ResetPasswordRoute
-} from '@/routes/public.routes';
-
-import Loader from '@/components/Shared/Loader';
+import { router } from '@/routes/router';
 
 import { useAuth } from '@/lib/hooks/useAuth';
 import { Verified } from '@/types';
-
-const routeTree = rootRoute.addChildren([
-  AuthRoute.addChildren([SigninRoute, SignupRoute, ForgotPasswordRoute, ResetPasswordRoute]),
-  MainRoute.addChildren([
-    HomeRoute,
-    ExploreRoute,
-    PeopleRoute,
-    CreatePostRoute,
-    PostDetailsRoute,
-    ProfileRoute.addChildren([UserPostsRoute, LikedPostsRoute, SavedPostsRoute])
-  ]),
-  NotFoundRoute
-]);
-
-const router = new Router({
-  routeTree,
-  defaultPendingComponent: Loader,
-  context: { queryClient, userID: undefined!, userAlias: undefined!, verified: undefined! }
-});
-
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router;
-  }
-}
 
 const verified: Verified = {
   isVerified: false,
